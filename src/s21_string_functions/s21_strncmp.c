cb18a6c2495109d653e58749c1ac27c433babbff
islam <itariverdievm@gmail.com>
2025-12-25 15:55:48 +0300

    s21_memset and s21_memcpy have started

--- src/s21_string_functions/s21_strncmp.c
+++ src/s21_string_functions/s21_strncmp.c
@@ -1,10 +1,13 @@
-#include "s21_string.h"
+#include "../s21_string.h"
 
-int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
     int result = 0;
     for (int i = 0; i < n; ++i) {
         if (((const char *)str1)[i] == ((const char *)str2)[i]) {
             continue;
+        }
+        if (((const char *)str1)[i] == '\0' && ((const char *)str2)[i] == '\0') {
+            break;
         }
         result = ((const char *)str1)[i] - ((const char *)str2)[i];
         break;
--- src/test_library.c
+++ src/test_library.c
@@ -4,17 +4,6 @@
 #include <string.h>
 
 int main() {
-    const char *str1 = "atoms\0\0\0\0";
-    const char *str2 = "atoms\0abc";
-    const char *str3 = "atomsaaa";
-
-    int result;
-    int myresult;
-
-    result = s21_strncmp(str1, str2, 8);
-    myresult = s21_memcmp(str1, str2, 8);
-    printf("%d\n", result);
-    printf("%d\n", myresult);
-
+    
     return 0;
 }
