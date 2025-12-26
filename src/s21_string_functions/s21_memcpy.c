f536191333df5f9c0082b99c6090c4a2e709c5c9
islam <itariverdievm@gmail.com>
2025-12-26 07:30:30 +0300

    s21_memset is done

--- src/s21_string_functions/s21_memcpy.c
+++ src/s21_string_functions/s21_memcpy.c
@@ -3,8 +3,7 @@
 
 void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
     for (s21_size_t i = 0; i < n; ++i) {
-        ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+        ((char *)dest)[i] = ((const char *)src)[i];
     }
-
-    return (void *)dest;
+    return dest;
 }
--- src/test_library.c
+++ src/test_library.c
@@ -4,6 +4,15 @@
 #include <string.h>
 
 int main() {
-    
+    int numbers[5];
+
+    s21_memset(numbers, 0, sizeof(numbers));
+
+    printf("Массив после memset(numbers, 0, sizeof(numbers)):\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+
     return 0;
 }
