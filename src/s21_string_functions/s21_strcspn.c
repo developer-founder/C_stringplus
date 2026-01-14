3760684e67c0be97fed838bbf15b5da2377f1eb3
islam <itariverdievm@gmail.com>
2026-01-14 09:54:48 +0300

    Промежуточные изменения

--- src/s21_string_functions/s21_strcspn.c
+++ src/s21_string_functions/s21_strcspn.c
@@ -1,5 +1,13 @@
 #include "../s21_string.h"
 
-s21_size_t s21_strcspn(const char* str1, const char* str2) {
-    
-}
+// s21_size_t s21_strcspn(const char* str1, const char* str2) {
+//     s21_size_t result = 0;
+
+//     if (str1) {
+//         while (str1[result] != '\0') {
+//             result++;
+//         }
+//     }
+
+//     return result;
+// }
--- src/tests/s21_tests.h
+++ src/tests/s21_tests.h
@@ -4,6 +4,7 @@
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
+#include <check.h>
 
 #include "../s21_string.h"
 
