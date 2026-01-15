39dca856e07a0eb2252660313bd5723702fa5b5e
islam <itariverdievm@gmail.com>
2026-01-15 23:13:56 +0300

    s21_strcspn tested, but is not work

--- src/s21_string_functions/s21_strcspn.c
+++ src/s21_string_functions/s21_strcspn.c
@@ -1,6 +1,16 @@
 #include "../s21_string.h"
-#include "string.h"
+#include <string.h>
 
 s21_size_t s21_strcspn(const char* str1, const char* str2) {
-    return (strcspn(str1, str2));
+    s21_size_t result = 0;
+
+    if (str1) {
+        while (str1[result] != '\0' && str1[result] != str2[result]) {
+            result++;
+        }
+    }
+
+    return result; 
+
+//    return strcspn(str1, str2);
 }
