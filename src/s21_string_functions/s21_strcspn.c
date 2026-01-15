c31748a980b20db0972c54a05510baf4845635ac
islam <itariverdievm@gmail.com>
2026-01-15 22:04:37 +0300

    Save current changes before switching to develop-ibragim

--- src/s21_string_functions/s21_strcspn.c
+++ src/s21_string_functions/s21_strcspn.c
@@ -1,4 +1,6 @@
 #include "../s21_string.h"
+#include "string.h"
 
-// s21_size_t s21_strcspn(const char* str1, const char* str2) {
-// }
+s21_size_t s21_strcspn(const char* str1, const char* str2) {
+  return (strcspn(str1, str2));
+}
