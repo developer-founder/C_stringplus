4e54575a7ff5ccda53f9b9aafcd45d37c3e20adc
islam <itariverdievm@gmail.com>
2026-01-17 07:41:00 +0300

    s21_strerror is done

--- src/s21_string.h
+++ src/s21_string.h
@@ -13,12 +13,16 @@ char *s21_strncat(char *dest, const char *src, s21_size_t n);
 char *s21_strchr(const char *str, int c);
 int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
 char *s21_strncpy(char *dest, const char *src, s21_size_t n);
-s21_size_t s21_strcspn(const char *str1, const char *str2);
-// char *s21_strerror(int errnum);
+s21_size_t s21_strcspn(const char *str1, const char *str2); // Проверен тестами
+char *s21_strerror(int errnum);
 s21_size_t s21_strlen(const char *str);
 // char *s21_strpbrk(const char *str1, const char *str2);
 // char *s21_strrchr(const char *str, int c);
 // char *s21_strstr(const char *haystack, const char *needle);
 // char *s21_strtok(char *str, const char *delim);
+// void *to_upper(const char *str);
+// void *to_lower(const char *str);
+// void *insert(const char *src, const char *str, size_t start_index);
+// void *trim(const char *src, const char *trim_chars);
 
 #endif
--- src/s21_string_functions/s21_strcspn.c
+++ src/s21_string_functions/s21_strcspn.c
@@ -1,5 +1,4 @@
 #include "../s21_string.h"
-#include <string.h>
 
 s21_size_t s21_strcspn(const char* str1, const char* str2) {
     s21_size_t result = 0;
