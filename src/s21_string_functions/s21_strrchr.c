504602841aac2813e97f54807ee3b0524871e641
islam <itariverdievm@gmail.com>
2026-01-17 16:26:53 +0300

    s21_strrchr is done, but it did not test

--- src/s21_string_functions/s21_strrchr.c
+++ src/s21_string_functions/s21_strrchr.c
@@ -1,9 +1,9 @@
 #include "../s21_string.h"
 
-char* s21_strchr(const char* str, int c) {
-    int i = 0;
+char* s21_strrchr(const char* str, int c) {
+    int i = s21_strlen(str) - 1;
     char* result = S21_NULL;
-    while (str[i] && str[i] != c) ++i;
+    while (str[i] && str[i] != c) --i;
     if (c == str[i]) {
         result = (char*)str + i;
     }
