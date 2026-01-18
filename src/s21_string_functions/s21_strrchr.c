ca6c487fc20fdebbcb922ee1fda561fb66f4618b
islam <itariverdievm@gmail.com>
2026-01-18 13:25:48 +0300

    s21_strchr was fixed

--- src/s21_string_functions/s21_strrchr.c
+++ src/s21_string_functions/s21_strrchr.c
@@ -1,11 +1,11 @@
 #include "../s21_string.h"
 
 char* s21_strrchr(const char* str, int c) {
-    int i = s21_strlen(str) - 1;
     char* result = S21_NULL;
-    while (str[i] && str[i] != c) --i;
-    if (c == str[i]) {
-        result = (char*)str + i;
+    for (s21_size_t i = 0; i < s21_strlen(str) + 1; ++i) {
+        if (c == str[i]) {
+            result = (char*)str + i;
+        }
     }
 
     return result;
