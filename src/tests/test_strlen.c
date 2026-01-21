e09469fe974689e33d019501698efcb93b6a8ca7
 admin <admin@MacBook-Pro-admin.local>
2026-01-21 05:54:45 +0300

    5 tests left

---
+++ src/tests/test_strlen.c
@@ -0,0 +1,244 @@
+#include "s21_tests.h"
+
+START_TEST(test_strlen_basic) {
+    char str[] = "Hello";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_empty) {
+    char str[] = "";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_long_string) {
+    char str[] = "This is a very long string to test the function properly";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_special_chars) {
+    char str[] = "Test\n\t\r\0";
+
+    // strlen остановится на первом '\0', не включая его
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_spaces) {
+    char str[] = "   ";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_mixed_spaces) {
+    char str[] = "  Hello World  ";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_numbers) {
+    char str[] = "1234567890";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_alphanumeric) {
+    char str[] = "Test123String456";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_one_char) {
+    char str[] = "A";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_unicode) {
+    char str[] = "Привет";  // 6 кириллических символов в UTF-8
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_null_in_middle) {
+    char str[] = {'H', 'e', 'l', 'l', 'o', '\0', 'W', 'o', 'r', 'l', 'd', '\0'};
+
+    // strlen остановится на первом '\0'
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_max_chars) {
+    char str[1025];
+
+    // Заполняем строку максимальной длины
+    for (int i = 0; i < 1024; i++) {
+        str[i] = 'A';
+    }
+    str[1024] = '\0';
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_repeated_char) {
+    char str[] = "aaaaaaaaaaaaaaaaaaaa";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_pattern) {
+    char str[] = "ABABABABABABABAB";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_whitespace_only) {
+    char str[] = "\n\t\r ";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_punctuation) {
+    char str[] = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_mixed_punctuation) {
+    char str[] = "Hello, World! How are you?";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_escape_chars) {
+    char str[] = "Line1\nLine2\tLine3\r";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_hex_chars) {
+    char str[] = "\x48\x65\x6C\x6C\x6F";  // "Hello" в hex
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_very_long) {
+    // Длинная строка, чтобы проверить производительность
+    char* str = malloc(10001);
+
+    for (int i = 0; i < 10000; i++) {
+        str[i] = 'A' + (i % 26);
+    }
+    str[10000] = '\0';
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+
+    free(str);
+}
+END_TEST
+
+START_TEST(test_strlen_only_null) {
+    char str[] = {'\0'};
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_concatenated) {
+    char str1[] = "Hello";
+    char str2[] = " World";
+    char result[20];
+
+    strcpy(result, str1);
+    strcat(result, str2);
+
+    ck_assert_int_eq(s21_strlen(result), strlen(result));
+}
+END_TEST
+
+START_TEST(test_strlen_after_strncpy) {
+    char src[] = "Hello";
+    char dest[10];
+
+    strncpy(dest, src, 5);
+    dest[5] = '\0';
+
+    ck_assert_int_eq(s21_strlen(dest), strlen(dest));
+}
+END_TEST
+
+START_TEST(test_strlen_pointer_arithmetic) {
+    char str[] = "Hello World";
+
+    // strlen от середины строки
+    ck_assert_int_eq(s21_strlen(str + 6), strlen(str + 6));
+}
+END_TEST
+
+START_TEST(test_strlen_const_string) {
+    const char* str = "Constant String";
+
+    ck_assert_int_eq(s21_strlen(str), strlen(str));
+}
+END_TEST
+
+START_TEST(test_strlen_literal) {
+    // Прямой вызов со строковым литералом
+    ck_assert_int_eq(s21_strlen("Test Literal"), strlen("Test Literal"));
+}
+END_TEST
+
+Suite* strlen_suites(void) {
+    Suite* strlen_suite = suite_create("strlen_tests");
+
+    TCase* all_strlen = tcase_create("Core");
+    tcase_add_test(all_strlen, test_strlen_basic);
+    tcase_add_test(all_strlen, test_strlen_empty);
+    tcase_add_test(all_strlen, test_strlen_long_string);
+    tcase_add_test(all_strlen, test_strlen_special_chars);
+    tcase_add_test(all_strlen, test_strlen_spaces);
+    tcase_add_test(all_strlen, test_strlen_mixed_spaces);
+    tcase_add_test(all_strlen, test_strlen_numbers);
+    tcase_add_test(all_strlen, test_strlen_alphanumeric);
+    tcase_add_test(all_strlen, test_strlen_one_char);
+    tcase_add_test(all_strlen, test_strlen_unicode);
+    tcase_add_test(all_strlen, test_strlen_null_in_middle);
+    tcase_add_test(all_strlen, test_strlen_max_chars);
+    tcase_add_test(all_strlen, test_strlen_repeated_char);
+    tcase_add_test(all_strlen, test_strlen_pattern);
+    tcase_add_test(all_strlen, test_strlen_whitespace_only);
+    tcase_add_test(all_strlen, test_strlen_punctuation);
+    tcase_add_test(all_strlen, test_strlen_mixed_punctuation);
+    tcase_add_test(all_strlen, test_strlen_escape_chars);
+    tcase_add_test(all_strlen, test_strlen_hex_chars);
+    tcase_add_test(all_strlen, test_strlen_very_long);
+    tcase_add_test(all_strlen, test_strlen_only_null);
+    tcase_add_test(all_strlen, test_strlen_concatenated);
+    tcase_add_test(all_strlen, test_strlen_after_strncpy);
+    tcase_add_test(all_strlen, test_strlen_pointer_arithmetic);
+    tcase_add_test(all_strlen, test_strlen_const_string);
+    tcase_add_test(all_strlen, test_strlen_literal);
+
+    suite_add_tcase(strlen_suite, all_strlen);
+
+    return strlen_suite;
+}
--- src/tests/test_strncpy.c
+++ src/tests/test_strncpy.c
@@ -1,89 +1,218 @@
 #include "s21_tests.h"
 
 START_TEST(test_strncpy_basic) {
-    char dest[20];
-    const char* src = "Hello World";
+    char src[] = "Hello";
+    char s21[20];
+    char original[20];
 
-    char* result = s21_strncpy(dest, src, 12);
-
-    ck_assert_ptr_eq(result, dest);
-    ck_assert_str_eq(dest, "Hello World");
-    ck_assert_int_eq(dest[11], '\0');  // Нуль-терминатор
+    s21_strncpy(s21, src, 5);
+    strncpy(original, src, 5);
+    s21[5] = '\0';
+    original[5] = '\0';
+    ck_assert_str_eq(s21, original);
 }
 END_TEST
 
-START_TEST(test_strncpy_exact_length) {
-    char dest[10];
-    const char* src = "123456789";
+START_TEST(test_strncpy_empty) {
+    char src[] = "";
+    char s21[20];
+    char original[20];
 
-    s21_strncpy(dest, src, 10);
-
-    // Все 9 символов + нуль-терминатор
-    ck_assert_str_eq(dest, "123456789");
+    s21_strncpy(s21, src, 5);
+    strncpy(original, src, 5);
+    ck_assert_str_eq(s21, original);
 }
 END_TEST
 
-START_TEST(test_strncpy_zero_count) {
-    char dest[10] = "abcdefghij";
-    const char* src = "Hello";
-    char original[10];
-    memcpy(original, dest, 10);
+START_TEST(test_strncpy_long) {
+    char src[] = "This is a longer test string";
+    char s21[50];
+    char original[50];
 
-    char* result = s21_strncpy(dest, src, 0);
+    s21_strncpy(s21, src, 10);
+    strncpy(original, src, 10);
+    s21[10] = '\0';
+    original[10] = '\0';
+    ck_assert_str_eq(s21, original);
+}
+END_TEST
 
-    ck_assert_ptr_eq(result, dest);
-    // dest не должен измениться при n = 0
-    ck_assert_mem_eq(dest, original, 10);
+START_TEST(test_strncpy_special_chars) {
+    char src[] = "Test\n\t\r";
+    char s21[20];
+    char original[20];
+
+    s21_strncpy(s21, src, 7);
+    strncpy(original, src, 7);
+    s21[7] = '\0';
+    original[7] = '\0';
+    ck_assert_str_eq(s21, original);
+}
+END_TEST
+
+START_TEST(test_strncpy_overwrite) {
+    char src[] = "New";
+    char s21[20] = "OldString";
+    char original[20] = "OldString";
+
+    s21_strncpy(s21, src, 3);
+    strncpy(original, src, 3);
+    /* strncpy не добавляет автоматически нуль-терминатор, если n <= strlen(src) */
+    ck_assert_mem_eq(s21, original, 3);
+    /* Проверяем, что остальная часть строки не изменилась */
+    ck_assert_str_eq(s21 + 3, "String");
 }
 END_TEST
 
 START_TEST(test_strncpy_empty_source) {
-    char dest[10] = "abcdefghij";
+    char s21[10] = "abcdefghij";
     const char* src = "";
+    char original[10] = "abcdefghij";
 
-    s21_strncpy(dest, src, 5);
+    s21_strncpy(s21, src, 5);
+    strncpy(original, src, 5);
 
-    // Первый символ должен быть '\0', остальные заполнены нулями до n
-    ck_assert_int_eq(dest[0], '\0');
-    ck_assert_int_eq(dest[1], '\0');
-    ck_assert_int_eq(dest[2], '\0');
-    ck_assert_int_eq(dest[3], '\0');
-    ck_assert_int_eq(dest[4], '\0');
-    // dest[5] и дальше не должны измениться
-    ck_assert_int_eq(dest[5], 'f');
+    // Первые 5 символов должны быть '\0'
+    for (int i = 0; i < 5; i++) {
+        ck_assert_int_eq(s21[i], '\0');
+        ck_assert_int_eq(original[i], '\0');
+    }
+    // s21[5] и дальше не должны измениться
+    ck_assert_int_eq(s21[5], 'f');
+    ck_assert_int_eq(original[5], 'f');
 }
 END_TEST
 
 START_TEST(test_strncpy_buffer_exact_size) {
-    char dest[6];
+    char s21[6];
+    char original[6];
     const char* src = "Hello";
 
-    // Правильное использование: n = sizeof(dest)
-    s21_strncpy(dest, src, sizeof(dest));
+    s21_strncpy(s21, src, sizeof(s21));
+    strncpy(original, src, sizeof(original));
 
-    // dest теперь гарантированно нуль-терминирован
-    ck_assert_str_eq(dest, "Hello");
+    // Обе функции должны заполнить весь буфер, включая нуль-терминаторы
+    ck_assert_mem_eq(s21, original, sizeof(s21));
+    // s21[5] должен быть '\0' (последний элемент массива)
+    ck_assert_int_eq(s21[5], '\0');
 }
 END_TEST
 
-START_TEST(test_strncpy_dest_equals_src) {
+START_TEST(test_strncpy_s21_equals_src) {
     char buffer[20] = "Test";
+    char buffer2[20] = "Test";
 
-    // Копирование в себя - технически разрешено если нет перекрытия
-    strncpy(buffer, buffer, 5);
+    s21_strncpy(buffer, buffer, 5);
+    strncpy(buffer2, buffer2, 5);
 
-    ck_assert_str_eq(buffer, "Test");
+    ck_assert_mem_eq(buffer, buffer2, 5);
 }
 END_TEST
 
 START_TEST(test_strncpy_partial_overwrite) {
-    char dest[20] = "Initial value";
+    char s21[20] = "Initial value";
+    char original[20] = "Initial value";
     const char* src = "New";
 
-    strncpy(dest, src, 3);
+    s21_strncpy(s21, src, 3);
+    strncpy(original, src, 3);
 
     // Только первые 3 символа изменены
-    ck_assert_mem_eq(dest, "Newial value", 12);
+    ck_assert_mem_eq(s21, original, 20);
+    ck_assert_mem_eq(s21, "Newial value", 11);
+}
+END_TEST
+
+START_TEST(test_strncpy_zero_count) {
+    char src[] = "Hello";
+    char s21[20] = "Test";
+    char original[20] = "Test";
+
+    s21_strncpy(s21, src, 0);
+    strncpy(original, src, 0);
+
+    // s21 не должен измениться
+    ck_assert_str_eq(s21, "Test");
+    ck_assert_str_eq(original, "Test");
+}
+END_TEST
+
+START_TEST(test_strncpy_count_greater_than_src) {
+    char src[] = "Hello";
+    char s21[20];
+    char original[20];
+
+    memset(s21, 'X', sizeof(s21));
+    memset(original, 'X', sizeof(original));
+
+    s21_strncpy(s21, src, 10);
+    strncpy(original, src, 10);
+
+    // Первые 5 символов - "Hello", остальные 5 - '\0'
+    ck_assert_mem_eq(s21, original, 10);
+    ck_assert_mem_eq(s21, "Hello", 5);
+    for (int i = 5; i < 10; i++) {
+        ck_assert_int_eq(s21[i], '\0');
+    }
+}
+END_TEST
+
+START_TEST(test_strncpy_exact_fit) {
+    char src[] = "ABCD";
+    char s21[5];
+    char original[5];
+
+    memset(s21, 'X', sizeof(s21));
+    memset(original, 'X', sizeof(original));
+
+    s21_strncpy(s21, src, 4);
+    strncpy(original, src, 4);
+
+    // s21[4] должен остаться 'X', так как n=4 не включает нуль-терминатор
+    ck_assert_mem_eq(s21, original, 5);
+    ck_assert_mem_eq(s21, "ABCD", 4);
+    ck_assert_int_eq(s21[4], 'X');
+}
+END_TEST
+
+START_TEST(test_strncpy_zero_length_with_null) {
+    char src[] = {'\0', 'A', 'B', 'C'};
+    char s21[10] = {0};
+    char original[10] = {0};
+
+    s21_strncpy(s21, src, 5);
+    strncpy(original, src, 5);
+
+    ck_assert_mem_eq(s21, original, 10);
+    ck_assert_int_eq(s21[0], '\0');
+    // Остальные 4 символа должны быть '\0'
+    for (int i = 1; i < 5; i++) {
+        ck_assert_int_eq(s21[i], '\0');
+    }
+}
+END_TEST
+
+START_TEST(test_strncpy_padding_zeros) {
+    char src[] = "Hi";
+    char s21[10];
+    char original[10];
+
+    memset(s21, 'A', sizeof(s21));
+    memset(original, 'A', sizeof(original));
+
+    s21_strncpy(s21, src, 5);
+    strncpy(original, src, 5);
+
+    // Проверяем что оставшиеся 3 байта заполнены нулями
+    ck_assert_mem_eq(s21, original, 10);
+    ck_assert_mem_eq(s21, "Hi", 2);
+    for (int i = 2; i < 5; i++) {
+        ck_assert_int_eq(s21[i], '\0');
+    }
+    // Позиции 5-9 должны остаться 'A'
+    for (int i = 5; i < 10; i++) {
+        ck_assert_int_eq(s21[i], 'A');
+    }
 }
 END_TEST
 
@@ -92,12 +221,19 @@ Suite* strncpy_suites(void) {
 
     TCase* all_strncpy = tcase_create("strncpy");
     tcase_add_test(all_strncpy, test_strncpy_basic);
-    tcase_add_test(all_strncpy, test_strncpy_exact_length);
-    tcase_add_test(all_strncpy, test_strncpy_zero_count);
+    tcase_add_test(all_strncpy, test_strncpy_empty);
+    tcase_add_test(all_strncpy, test_strncpy_long);
+    tcase_add_test(all_strncpy, test_strncpy_special_chars);
+    tcase_add_test(all_strncpy, test_strncpy_overwrite);
     tcase_add_test(all_strncpy, test_strncpy_empty_source);
     tcase_add_test(all_strncpy, test_strncpy_buffer_exact_size);
-    tcase_add_test(all_strncpy, test_strncpy_dest_equals_src);
+    tcase_add_test(all_strncpy, test_strncpy_s21_equals_src);
     tcase_add_test(all_strncpy, test_strncpy_partial_overwrite);
+    tcase_add_test(all_strncpy, test_strncpy_zero_count);
+    tcase_add_test(all_strncpy, test_strncpy_count_greater_than_src);
+    tcase_add_test(all_strncpy, test_strncpy_exact_fit);
+    tcase_add_test(all_strncpy, test_strncpy_zero_length_with_null);
+    tcase_add_test(all_strncpy, test_strncpy_padding_zeros);
 
     suite_add_tcase(strncpy_suite, all_strncpy);
 
